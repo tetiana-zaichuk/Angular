d1c0033ff63018da8291fbfa069689ba787e1082
tetiana-zaichuk <tanyazaychuk@gmail.com>
2018-07-27 20:07:52 +0300

    Added navigation.component

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -6,38 +6,30 @@ import { AppComponent } from './app.component';
 
 import { AircraftTypeListComponent } from './aircraft-type-list/aircraft-type-list.component';
 import { AircraftTypeDetailComponent } from './aircraft-type-list/aircraft-type-detail/aircraft-type-detail.component';
-
 import { AircraftListComponent } from './aircraft-list/aircraft-list.component';
 import { AircraftDetailComponent } from './aircraft-list/aircraft-detail/aircraft-detail.component';
-
 import { CrewListComponent } from './crew-list/crew-list.component';
 import { CrewDetailComponent } from './crew-list/crew-detail/crew-detail.component';
-
 import { DepartureListComponent } from './departure-list/departure-list.component';
 import { DepartureDetailComponent } from './departure-list/departure-detail/departure-detail.component';
-
 import { FlightListComponent } from './flight-list/flight-list.component';
 import { FlightDetailComponent } from './flight-list/flight-detail/flight-detail.component';
-
 import { PilotListComponent } from './pilot-list/pilot-list.component';
 import { PilotDetailComponent } from './pilot-list/pilot-detail/pilot-detail.component';
-
 import { TicketListComponent } from './ticket-list/ticket-list.component';
 import { TicketDetailComponent } from './ticket-list/ticket-detail/ticket-detail.component';
-
 import { StewardessListComponent } from './stewardess-list/stewardess-list.component';
 import { StewardessDetailComponent } from './stewardess-list/stewardess-detail/stewardess-detail.component';
-
 import { ServicesEightEntitiesModule } from './services-eight-entities/services-eight-entities.module';
-
 import { AppRoutingModule } from './/app-routing.module';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule, HttpClientModule, AppRoutingModule, ServicesEightEntitiesModule],
   declarations: [AppComponent, AircraftTypeListComponent, AircraftTypeDetailComponent, AircraftListComponent, 
     AircraftDetailComponent, CrewDetailComponent, DepartureDetailComponent, FlightDetailComponent, PilotDetailComponent, 
     TicketDetailComponent, StewardessDetailComponent, CrewListComponent, DepartureListComponent, FlightListComponent, 
-    PilotListComponent, TicketListComponent, StewardessListComponent],
+    PilotListComponent, TicketListComponent, StewardessListComponent, NavigationComponent],
   bootstrap: [AppComponent],
   providers: []
 })
